3b91cf202564d121a6de83c3e740b0deba60b2dd
mylmz10 <yilmaz.muammer@outlook.com>
2018-12-18 14:12:57 +0300

    init project

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <Label class="header" text="Header" :textWrap="true" />
+</template>
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/variables';
+
+.header{
+    color: $text-color;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,2 @@
+// Components
+export { default as Header } from './components/Header';
